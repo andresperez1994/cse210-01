6ebcd2c7cd07bc1f8c7efd467a87d78c62ee2852
andresperez1994 <97799396+andresperez1994@users.noreply.github.com>
2022-01-16 00:14:32 -0500

    Delete 01.py

--- 01.py
+++
@@ -1,8 +0,0 @@
-#este es el primero codigo
-print("este es el primer codigo")
-print("nuevo código")
-print("nuevo codigo 2 ")
-print("nuevo codigo 3")
-print("nuevo codigo 4")
-print("nuevo codigo 5")
-print("nuevo codigo 6")
